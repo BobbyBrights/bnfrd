883380772a6ec53e1dc6f18ecee9fd67ef91c6aa
Benjamin Ford <beford@beford665639.starbucks.net>
2018-05-03 10:14:20 -0700

    its alive!

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <div class="row header middle">
-    <div class="first"> <h1><a href="index.php">Ben Ford&nbsp;</a><strong>is a product designer</strong></h1></div>
-    <div class="last"> <h1 class="black-secondary"><a href="about.php">About</a></h1></div>
+    <div class="first"> <h1><a href="index">Ben Ford&nbsp;</a><strong>is a product designer</strong></h1></div>
+    <div class="last"> <h1 class="black-secondary"><a href="about">About</a></h1></div>
 </div>
