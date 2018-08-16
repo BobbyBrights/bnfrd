3f5c0c291dfdbea22452840a85ff5bd79e73a37a
Benjamin Ford <beford@beford665639.starbucks.net>
2018-08-16 11:25:32 -0700

    sbux work

---
+++ sbux/index.php
@@ -0,0 +1,372 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>App Playground</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Facebook / Google meta info -->
+	<link rel="canonical" href="http://www.website.com/" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Website Name Here" />
+	<meta property="og:description" content="Description of website here" />
+	<meta property="og:url" content="http://www.website.com/" />
+	<meta property="og:site_name" content="Website name here" />
+	<meta property="og:image" content="/img/facebook-share.png" />
+
+	<!-- Twitter Meta info -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="http://www.website.com/" />
+	<meta name="twitter:title" content="Website name here" />
+	<meta name="twitter:description" content="Description of website here" />
+	<meta name="twitter:image" content="/img/facebook-share.png" />
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+	<meta name="application-name" content="&nbsp;"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="/img/mstile-144x144.png" />
+
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="style.css" media="all">
+	<!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" media="all"> -->
+
+
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="left-crate">
+      <div class="global-nav c12">
+        <div class="siren">
+        </div>
+      </div>
+    </div>
+    <div class="right-crate pb3">
+      <div class="right-grid">
+        <!-- <div class="card c12">d
+        </div> -->
+
+					<div class="product-cell jjk c12">
+	          <div class="title-row c12">
+	            <div class="product-img">
+	            </div>
+	            <div class="product-title">
+	              Latte Macchiato
+	            </div>
+	          </div>
+	          <div class="detail-row c12">
+	            <div class="descriptor">Grande
+	            </div>
+	          </div>
+	          <div class="detail-row c12 pt1">
+	            <div class="descriptor">2 pumps vanilla
+	            </div>
+	            <div class="dot"></div>
+	            <div class="price">$0.50
+	            </div>
+	          </div>
+						<div class="detail-row c12 pt1">
+							<div class="descriptor">Costs 125<span class="gold">★</span>
+							</div>
+						</div>
+	          <div class="detail-row c12 pt2">
+	            <div class="descriptor red-text bold">Sold out at this store
+	            </div>
+	            <div class="warning">
+	            </div>
+	          </div>
+	          <div class="utility-row c12">
+	            <div class="utility-actions">
+	              <div class="heart">
+	              </div>
+	              <div class="plus">
+	              </div>
+	              <div class="minus">
+	              </div>
+	            </div>
+	            <div class="total-price">$3.95
+	            </div>
+	          </div>
+	          <div class="rewards-row c12">
+	            <div class="birthday-row">
+	              <div class="ghost birthday-button"><span class="birthday-action">Birthday Reward</span>
+	              </div>
+								<div class="ghost"><span class="reward-action">Use</span> 25<span class="gold">★</span>
+	              </div>
+	              <div class="ghost"><span class="reward-action">Use</span> 150<span class="gold">★</span>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	        <hr>
+	        <div class="product-cell c12">
+	          <div class="title-row c12">
+	            <div class="product-img2">
+	            </div>
+	            <div class="product-title">
+	              Double-Smoked Bacon, Cheddar & Egg Sandwich
+	            </div>
+	          </div>
+	          <div class="detail-row c12">
+	            <div class="descriptor">Warmed
+	            </div>
+	          </div>
+						<div class="detail-row c12 pt1 pb2">
+							<div class="descriptor">Costs 150<span class="gold">★</span>
+							</div>
+						</div>
+	          <div class="utility-row c12">
+	            <div class="utility-actions">
+	              <div class="heart">
+	              </div>
+	              <div class="plus">
+	              </div>
+	              <div class="minus">
+	              </div>
+	            </div>
+	            <div class="total-price">$3.95
+	            </div>
+	          </div>
+						<div class="rewards-row">
+	            <div class="birthday-row c12">
+	              <div class="ghost"><span class="reward-action">Use</span> 150<span class="gold">★</span>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	        <hr>
+	        <div class="product-cell c12">
+	          <div class="title-row c12">
+	            <div class="product-img3">
+	            </div>
+	            <div class="product-title">
+	              Espresso Roast Verismo® Pods
+	            </div>
+	          </div>
+	          <div class="detail-row c12">
+	            <div class="descriptor">12 count
+	            </div>
+	          </div>
+						<div class="detail-row c12 pt1 pb2">
+							<div class="descriptor">Costs 400<span class="gold">★</span>
+							</div>
+						</div>
+	          <div class="utility-row c12">
+	            <div class="utility-actions">
+	              <div class="heart">
+	              </div>
+	              <div class="plus">
+	              </div>
+	              <div class="minus">
+	              </div>
+	            </div>
+	            <div class="total-price">$3.95
+	            </div>
+	          </div>
+	          <div class="rewards-row">
+	            <div class="birthday-row c12">
+	              <div class="ghost"><span class="reward-action">Use</span> 400<span class="gold">★</span>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	        <hr>
+	        <div class="product-cell c12">
+	          <div class="title-row c12">
+	            <div class="product-img4">
+	            </div>
+	            <div class="product-title">
+	              Chai Crème Frappuccino®
+	            </div>
+	          </div>
+	          <div class="detail-row c12">
+	            <div class="descriptor">Venti
+	            </div>
+	          </div>
+	          <div class="detail-row c12 pt1">
+	            <div class="descriptor">1 pump mocha sauce
+	            </div>
+	            <div class="dot"></div>
+	            <div class="price">$0.25
+	            </div>
+	          </div>
+						<div class="detail-row c12 pt1 pb2">
+							<div class="descriptor">Costs 150<span class="gold">★</span>
+							</div>
+						</div>
+	          <div class="utility-row c12">
+	            <div class="utility-actions">
+	              <div class="heart">
+	              </div>
+	              <div class="plus">
+	              </div>
+	              <div class="minus">
+	              </div>
+	            </div>
+	            <div class="total-price">$3.95
+	            </div>
+	          </div>
+						<div class="rewards-row">
+	            <div class="birthday-row c12">
+	              <div class="ghost"><span class="reward-action">Use</span> 150<span class="gold">★</span>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	        <hr>
+	        <div class="pricing-cell c12 mb4">
+	          <div class="detail-row c12 pt1 pb2">
+	            <div class="descriptor">Subtotal
+	            </div>
+	            <div class="dot"></div>
+	            <div class="price">$5.47
+	            </div>
+	          </div>
+	          <div class="detail-row c12 pb2">
+	            <div class="descriptor">Tax 10.1%
+	            </div>
+	            <div class="dot"></div>
+	            <div class="price">$1.18
+	            </div>
+	          </div>
+	          <div class="detail-row c12 pb2">
+	            <div class="descriptor">Tip
+	            </div>
+	            <div class="dot"></div>
+	            <div class="price">$0.00
+	            </div>
+	          </div>
+	          <div class="detail-row c12 pb2">
+	            <div class="descriptor">Charity Roundup
+	            </div>
+	            <div class="dot"></div>
+	            <div class="price">$0.50
+	            </div>
+	          </div>
+	          <div class="detail-row c12">
+	            <div class="descriptor large-text">Total
+	            </div>
+	            <div class="dot"></div>
+	            <div class="price large-text">$6.50
+	            </div>
+	          </div>
+	        </div>
+        <div class="frap">Checkout
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+<!-- JS -->
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
+<script type='text/javascript' src='/js/main.js'></script>
+
+<script>
+
+// $('.birthday-button').click( function() {
+//     $(".birthday-price").toggleClass("birthday-active");
+// } );
+
+// $('.birthday-button').click( function() {
+//     $(".birthday-price").toggleClass("hide");
+// } );
+
+
+$(".ghost").click(function() {
+    $(".ghost").not(this).toggleClass("hide");
+
+    var rewardText = $(this).find('.reward-action').text();
+    if (rewardText === "Use") {
+        $(this).find('.reward-action').text("Remove");
+    } else {
+        $(this).find('.reward-action').text("Use");
+    }
+
+		var birthdayText = $(this).find('.birthday-action').text();
+    if (birthdayText === "Birthday Reward") {
+        $(this).find('.birthday-action').text("Remove Birthday");
+    } else {
+        $(this).find('.birthday-action').text("Birthday Reward");
+    }
+});
+
+// $(".minus").click(function() {
+//     $(".jjk").toggleClass("scale-out-center");
+// 		});
+
+// $(".minus").click(function() {
+//     $(".jjk").toggleClass("scale-out-center", function() {
+// 				$(this).remove();
+//     });
+// });
+//
+// $(".minus").click(function() {
+//   $(this).parents(".product-cell").toggleClass("scale-out-center").delay(1000).hide();
+// 	console.log("hi!");
+// });
+
+
+$(".minus").click(function() {
+$(this).parents(".product-cell").toggleClass("scale-out-center").delay(200).queue(function() {
+                           $(this).remove();
+                           $(this).dequeue();
+                       });
+											 });
+
+
+// $(".minus").click(function() {
+// $(".jjk").fadeOut(300, function(){
+//     $(this).remove();
+// });
+
+// $(".notificationClose").click(function() {
+//     $("#notification").fadeOut("normal", function() {
+//         $(this).remove();
+//     });
+// });
+
+
+
+
+
+// $(document).ready(function() {
+//   $('.birthday-button').click(myFunction);
+// });
+//
+// function myFunction() {
+//   alert('hi');
+// }
+//
+// function myFunction() {
+// 	alert('hi');
+//   var x = document.getElementsByClassName('');
+//   if (x.innerHTML === "Birthday Reward") {
+//     x.innerHTML = "Remove Birthday";
+//   } else {
+//     x.innerHTML = "Birthday Reward";
+//   }
+// }
+
+
+
+
+</script>
+
+<!-- Analytics -->
+
+</body>
+</html>
