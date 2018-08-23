07c560d5a76556bbde4ac892a00e8c828c2f3cf7
Benjamin Ford <beford@beford665639.starbucks.net>
2018-08-23 16:22:50 -0700

    more button work

--- sbux/button-animations/footer.php
+++ sbux/button-animations/footer.php
@@ -5,42 +5,45 @@
 
 <script>
 
-(function (window, $) {
+// Material Ripple effect
+$(".material-ripple").click(function(event) {
+	var surface = $(this);
 
-  $(function() {
+	// create .material-ink element if doesn't exist
+	if (surface.find(".material-ink").length == 0) {
+		surface.prepend("<div class='material-ink'></div>");
+	}
 
+	var ink = surface.find(".material-ink");
 
-    $('.ripple').on('click', function (event) {
-      event.preventDefault();
+	// in case of quick double clicks stop the previous animation
+	ink.removeClass("animate");
 
-      var $div = $('<div/>'),
-          btnOffset = $(this).offset(),
-      		xPos = event.pageX - btnOffset.left,
-      		yPos = event.pageY - btnOffset.top;
+	// set size of .ink
+	if (!ink.height() && !ink.width()) {
+		// use surface's width or height whichever is larger for
+		// the diameter to make a circle which can cover the entire element
+		var diameter = Math.max(surface.outerWidth(), surface.outerHeight());
+		ink.css({height: diameter, width: diameter});
+	}
 
+	// get click coordinates
+	// Logic:
+	// click coordinates relative to page minus
+	// surface's position relative to page minus
+	// half of self height/width to make it controllable from the center
+	var xPos = event.pageX - surface.offset().left - (ink.width() / 2);
+	var yPos = event.pageY - surface.offset().top - (ink.height() / 2);
 
+	var rippleColor = surface.data("ripple-color");
 
-      $div.addClass('ripple-effect');
-      var $ripple = $(".ripple-effect");
-
-      $ripple.css("height", $(this).height());
-      $ripple.css("width", $(this).height());
-      $div
-        .css({
-          top: yPos - ($ripple.height()/2),
-          left: xPos - ($ripple.width()/2),
-          background: $(this).data("ripple-color")
-        })
-        .appendTo($(this));
-
-      window.setTimeout(function(){
-        $div.remove();
-      }, 2000);
-    });
-
-  });
-
-})(window, jQuery);
+	//set the position and add class .animate
+	ink.css({
+		top: yPos + 'px',
+		left: xPos + 'px',
+		background: rippleColor
+	}).addClass("animate");
+});
 
 </script>
 
--- sbux/button-animations/right-crate.php
+++ sbux/button-animations/right-crate.php
@@ -17,50 +17,46 @@
 		</div>
 
 		<div class="card hcenter mt3 row cardCorner cardShadow colorWhite c12">
-			<div class="heart2"><a href="#"></a>
+			<div class="heart2 material-ripple ripple-color"><a href="#"></a>
 				<div class="heart-hover2"></div>
 				<div class="heart-img2"></div>
 			</div>
-			<div class="plus2"><a href="#"></a>
+			<div class="plus2 material-ripple ripple-color"><a href="#"></a>
 				<div class="plus-hover2"></div>
 				<div class="plus-img2"></div>
 			</div>
-			<div class="minus2"><a href="#"></a>
+			<div class="minus2 material-ripple ripple-color"><a href="#"></a>
 				<div class="minus-hover2"></div>
 				<div class="minus-img2"></div>
 			</div>
 		</div>
 
 		<div class="card hcenter mt3 row cardCorner cardShadow colorWhite c12">
-			<button class="heart3"><a href="#"></a>
+			<div class="heart3"><a href="#"></a>
 				<div class="heart-hover3"></div>
 				<div class="heart-img3"></div>
-			</button>
-			<button class="plus3"><a href="#"></a>
+			</div>
+			<div class="plus3"><a href="#"></a>
 				<div class="plus-hover3"></div>
 				<div class="plus-img3"></div>
-			</button>
-			<button class="minus3"><a href="#"></a>
+			</div>
+			<div class="minus3"><a href="#"></a>
 				<div class="minus-hover3"></div>
 				<div class="minus-img3"></div>
-			</button>
+			</div>
 
 		</div>
 
 		<div class="card hcenter mt3 row cardCorner cardShadow colorWhite c12">
-			<button class ="heart3" data-ripple-color="#89669b" style="background:white; color:black;"><a href="#"></a>
-				<div class="heart-hover3"></div>
-				<div class="heart-img3"></div>
-			</button>
-			<button class="plus3"><a href="#"></a>
-				<div class="plus-hover3"></div>
-				<div class="plus-img3"></div>
-			</button>
-			<button class="minus3"><a href="#"></a>
-				<div class="minus-hover3"></div>
-				<div class="minus-img3"></div>
-			</button>
+			<button class="but1 ghost material-ripple ripple-color">Learn More </button>
+			<button class="but1 ghost-filled material-ripple ripple-color-white ml3">Learn More </button>
+			<button class="but1 ghost-text material-ripple ripple-color ml3">Learn More </button>
+		</div>
 
+		<div class="card hcenter mt3 row cardCorner cardShadow colorWhite c12">
+			<button class="but2 ghost2 material-ripple ripple-color">Learn More </button>
+			<button class="but2 ghost-filled2 material-ripple ripple-color-white ml3">Learn More </button>
+			<button class="but2 ghost-text2 ml3"><div class="mini-box"></div>Learn More </button>
 		</div>
 	</div>
 </div>
