c84a540621a3f1473b201a76cddae1b684a1e5f4
Benjamin Ford <beford@beford665639.starbucks.net>
2018-08-24 10:53:39 -0700

    dsfsfsd

--- sbux/button-animations/index.php
+++ sbux/button-animations/index.php
@@ -5,24 +5,75 @@
 
 <!-- Left Crate -->
   <div class="container">
-    <div class="sb-headerCrate colorWhite">
-      <div class="sb-navbar">
-          <div class="sb-navbar-siren vcenter"></div>
-          <nav class="row vcenter between c12">
-            <ul class="row vcenter text-noUnderline text-upper text-bold text-xxs">
-              <li class="py2 m1 pl2">Order</li>
-              <li class="py2 m1 pl2">Cards</li>
-              <li class="py2 m1 pl2">Gift</li>
-              <li class="py2 m1 pl2">Stores</li>
-            </ul>
-          <div class="hamburger vcenter colorWhite">
-            <div class="hamburger-img"></div>
-          </div>
-        </nav>
-      </div>
-      <div class="greeting sb-heading sb-heading--large text-bold">Good Morning, Ben🍻</div>
-    </div>
-		<?php include 'right-crate.php';?>
+		<?php include 'left-crate.php';?>
+
+		<!-- Right Crate -->
+		<div class="sb-contentCrate pt6">
+			<div class="sb-contentCrateContainer">
+				<p class="text-noUnderline text-upper text-bold text-xxs mb2">Gmail-esque</p>
+				<div class="card hcenter row cardCorner cardShadow colorWhite c12">
+					<div class="heart"><a href="#"></a>
+						<div class="heart-hover"></div>
+						<div class="heart-img"></div>
+					</div>
+					<div class="plus"><a href="#"></a>
+						<div class="plus-hover"></div>
+						<div class="plus-img"></div>
+					</div>
+					<div class="minus"><a href="#"></a>
+						<div class="minus-hover"></div>
+						<div class="minus-img"></div>
+					</div>
+				</div>
+
+				<p class="text-noUnderline mt4 text-upper text-bold text-xxs mb2">Ripple & Scale</p>
+				<div class="card hcenter row cardCorner cardShadow colorWhite c12">
+					<div class="heart2 material-ripple ripple-color"><a href="#"></a>
+						<div class="heart-hover2"></div>
+						<div class="heart-img2"></div>
+					</div>
+					<div class="plus2 material-ripple ripple-color"><a href="#"></a>
+						<div class="plus-hover2"></div>
+						<div class="plus-img2"></div>
+					</div>
+					<div class="minus2 material-ripple ripple-color"><a href="#"></a>
+						<div class="minus-hover2"></div>
+						<div class="minus-img2"></div>
+					</div>
+				</div>
+
+				<p class="text-noUnderline mt4 text-upper text-bold text-xxs mb2">Scale only</p>
+				<div class="card hcenter row cardCorner cardShadow colorWhite c12">
+					<div class="heart3"><a href="#"></a>
+						<div class="heart-hover3"></div>
+						<div class="heart-img3"></div>
+					</div>
+					<div class="plus3"><a href="#"></a>
+						<div class="plus-hover3"></div>
+						<div class="plus-img3"></div>
+					</div>
+					<div class="minus3"><a href="#"></a>
+						<div class="minus-hover3"></div>
+						<div class="minus-img3"></div>
+					</div>
+
+				</div>
+
+				<p class="text-noUnderline mt4 text-upper text-bold text-xxs mb2">Ripple & Scale on Click</p>
+				<div class="card hcenter row cardCorner cardShadow colorWhite c12">
+					<button class="but1 ghost material-ripple ripple-color">Learn More </button>
+					<button class="but1 ghost-filled material-ripple ripple-color-white ml3">Learn More </button>
+					<button class="but1 ghost-text material-ripple ripple-color ml3">Learn More </button>
+				</div>
+
+				<p class="text-noUnderline mt4 text-upper text-bold text-xxs mb2">DropShadow, Ripple, Scale on Click</p>
+				<div class="card hcenter row cardCorner cardShadow colorWhite c12">
+					<button class="but2 ghost2 material-ripple ripple-color">Learn More </button>
+					<button class="but2 ghost-filled2 material-ripple ripple-color-white ml3">Learn More </button>
+					<button class="but2 ghost-text2 material-ripple ripple-color ml3"><div class="mini-box"></div>Learn More </button>
+				</div>
+			</div>
+		</div>
   </div>
 
 <?php include 'footer.php';?>
---
+++ sbux/button-animations/left-crate.php
@@ -0,0 +1,17 @@
+<div class="sb-headerCrate colorWhite">
+  <div class="sb-navbar">
+      <div class="sb-navbar-siren vcenter"></div>
+      <nav class="row vcenter between c12">
+        <ul class="row vcenter text-noUnderline text-upper text-bold text-xxs">
+          <li class="py2 m1 pl2">Order</li>
+          <li class="py2 m1 pl2">Cards</li>
+          <li class="py2 m1 pl2">Gift</li>
+          <li class="py2 m1 pl2">Stores</li>
+        </ul>
+      <div class="hamburger vcenter colorWhite">
+        <div class="hamburger-img"></div>
+      </div>
+    </nav>
+  </div>
+  <div class="greeting sb-heading sb-heading--large text-bold">Good Morning, Ben🍻</div>
+</div>
