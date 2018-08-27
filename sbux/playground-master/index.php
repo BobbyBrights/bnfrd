476641fcf4507644383ae6afacc6082ec20515ab
Benjamin Ford <beford@beford665639.starbucks.net>
2018-08-27 16:20:46 -0700

    updated playground master

--- sbux/playground-master/index.php
+++ sbux/playground-master/index.php
@@ -1,112 +1,17 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-	<title>App Playground</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-	<!-- Facebook / Google meta info -->
-	<link rel="canonical" href="http://www.website.com/" />
-	<meta property="og:locale" content="en_US" />
-	<meta property="og:type" content="website" />
-	<meta property="og:title" content="Website Name Here" />
-	<meta property="og:description" content="Description of website here" />
-	<meta property="og:url" content="http://www.website.com/" />
-	<meta property="og:site_name" content="Website name here" />
-	<meta property="og:image" content="/img/facebook-share.png" />
-
-	<!-- Twitter Meta info -->
-	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:site" content="http://www.website.com/" />
-	<meta name="twitter:title" content="Website name here" />
-	<meta name="twitter:description" content="Description of website here" />
-	<meta name="twitter:image" content="/img/facebook-share.png" />
-
-	<!-- Favicon -->
-  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/apple-touch-icon-57x57.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="img/apple-touch-icon-60x60.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="img/apple-touch-icon-120x120.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="img/apple-touch-icon-76x76.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
-  <link rel="icon" type="image/png" href="img/favicon-196x196.png" sizes="196x196" />
-  <link rel="icon" type="image/png" href="img/favicon-96x96.png" sizes="96x96" />
-  <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16" />
-  <link rel="icon" type="image/png" href="img/favicon-128.png" sizes="128x128" />
-  <meta name="application-name" content="&nbsp;"/>
-  <meta name="msapplication-TileColor" content="#FFFFFF" />
-  <meta name="msapplication-TileImage" content="img/mstile-144x144.png" />
-  <meta name="msapplication-square70x70logo" content="img/mstile-70x70.png" />
-  <meta name="msapplication-square150x150logo" content="img/mstile-150x150.png" />
-  <meta name="msapplication-wide310x150logo" content="img/mstile-310x150.png" />
-  <meta name="msapplication-square310x310logo" content="img/mstile-310x310.png" />
-
-
-	<!-- Styles -->
-	<link rel="stylesheet" type="text/css" href="style/style.css" media="all">
-	<!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" media="all"> -->
-
-
-</head>
-
+<?php include 'header.php';?>
 <body>
 
 <!-- Left Crate -->
   <div class="container">
-    <div class="sb-headerCrate colorWhite">
-      <div class="sb-navbar">
-          <div class="sb-navbar-siren vcenter"></div>
-          <nav class="row vcenter between c12">
-            <ul class="row vcenter text-noUnderline text-upper text-bold text-xxs">
-              <li class="py2 m1 pl2">Order</li>
-              <li class="py2 m1 pl2">Cards</li>
-              <li class="py2 m1 pl2">Gift</li>
-              <li class="py2 m1 pl2">Stores</li>
-            </ul>
-          <div class="hamburger vcenter colorWhite">
-            <div class="hamburger-img"></div>
-          </div>
-        </nav>
-      </div>
-      <div class="greeting sb-heading sb-heading--large text-bold">Good Morning, Ben🍻</div>
-    </div>
-
+		<?php include 'left-crate.php';?>
 
 		<!-- Right Crate -->
-    <div class="sb-contentCrate">
+		<div class="sb-contentCrate">
 			<div class="sb-contentCrateContainer">
-				<div class="card row cardCorner cardShadow colorWhite c12">
-					kfgmskdfn klsd fmksdlfnlksd fndslflkds fknsdf sdln lksdfn klsdnf lskdnfsd lkfnds lfnlkfndslk fkl dfjls dflksd fjkl fklsdfdsfdsfsdfdsfsdfdfsd
-				</div>
-				<div class="card row cardCorner cardShadow colorWhite c12 mt3">
-					kfgmskdfn klsd fmksdlfnlksd fndslflkds fknsdf sdln lksdfn klsdnf lskdnfsd lkfnds lfnlkfndslk fkl dfjls dflksd fjkl fklsdfdsfdsfsdfdsfsdfdfsd
-				</div>
-				<div class="card row cardCorner cardShadow colorWhite c12 mt3">
-					kfgmskdfn klsd fmksdlfnlksd fndslflkds fknsdf sdln lksdfn klsdnf lskdnfsd lkfnds lfnlkfndslk fkl dfjls dflksd fjkl fklsdfdsfdsfsdfdsfsdfdfsd
-				</div>
 			</div>
-    </div>
+		</div>
   </div>
 
-
-
-
-
-<!-- JS -->
-<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
-<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
-<script type='text/javascript' src='/js/main.js'></script>
-
-<script>
-
-// Javascript & jQuery goes here
-
-</script>
-
-
-
-</body>
-</html>
+<?php include 'footer.php';?>
