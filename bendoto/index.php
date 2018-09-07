676bfdb72444ac3a79b039a77a8177ea9b2f04a9
Benjamin Ford <beford@beford665639.starbucks.net>
2018-09-07 14:40:26 -0700

    starting new website

---
+++ bendoto/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Ben Ford – Product Designer</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Facebook / Google meta info -->
+	<link rel="canonical" href="http://www.website.com/" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Ben Ford – Product Designer" />
+	<meta property="og:description" content="Portfolio of Ben Ford, a Product Designer" />
+	<meta property="og:url" content="http://www.website.com/" />
+	<meta property="og:site_name" content="Ben Ford – Product Designer" />
+	<meta property="og:image" content="/img/facebook-share.png" />
+
+	<!-- Twitter Meta info -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="http://www.website.com/" />
+	<meta name="twitter:title" content="Website name here" />
+	<meta name="twitter:description" content="Description of website here" />
+	<meta name="twitter:image" content="/img/facebook-share.png" />
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+	<meta name="application-name" content="&nbsp;"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="/img/mstile-144x144.png" />
+
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="style/utility-classes.css" media="all">
+  <link rel="stylesheet" type="text/css" href="style/style.css" media="all">
+	<!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" media="all"> -->
+
+
+</head>
+
+<body>
+  <!-- Global Nav Start -->
+    <!-- <header>
+      <nav class="row">
+        <div class="nav-brand vcenter body-2 p3">
+          Ben Ford
+        </div>
+        <h3 class="body-2 ml-auto p4">
+          About
+        </h3>
+      </nav>
+    </header> -->
+
+  <!-- Project Intro Start -->
+    <section class="full-width">
+      <div class="grid screen-combo relative py7">
+
+          <div class="row">
+            <div class="sm-c12 md-c7 relative md-last">
+              <img class="web-app-img-3" src="/img/web-app/screen-combo.png" alt="a delicious iced macchiatto">
+              <!-- <img class="web-app-img-2" src="/img/web-app/screen-1.png" alt="a computer showing a product page"> -->
+            </div>
+            <div class="sm-c12 md-c5 vcenter pr3">
+              <h1 class="heading-2 colorTextPrimary">
+                Starbucks PWA
+              </h1>
+              <p class="body-1 pt3 colorTextSecondary">
+                Starbucks had an online presence that was largely informational
+                and inconsistent with its native apps. With web seeing more than
+                double the traffic of its native apps, we saw an opportunity to
+                bring mobile ordering and other successful native features to the web.
+              </p>
+              <!-- <div class="box">
+                <div class="c6 pt7">
+                  <h3 class="caption colorTextPrimary">
+                    Role
+                  </h3>
+                  <p class="body-2 pt2 colorTextSecondary">
+                    Web Design / UX Design
+                  </p>
+                </div>
+                <div class="c6 pt7">
+                  <h3 class="caption colorTextPrimary">
+                    Credits
+                  </h3>
+                  <p class="body-2 pt2 colorTextSecondary">
+                    Jeremy Beasley, Carlyn Levy, Andrew Nedimeyer, Cam Searcy
+                  </p>
+                </div>
+              </div> -->
+            </div>
+          </div>
+
+
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- JS -->
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
+<script type='text/javascript' src='/js/main.js'></script>
+
+<script>
+</script>
+
+</body>
+</html>
