fd58792a43cf083294f4e5b35bffcf12f2533d2c
Benjamin Ford <beford@beford665639.starbucks.net>
2018-09-17 23:55:52 -0700

    more code cleanup

---
+++ bendoto/javascript.php
@@ -0,0 +1,7 @@
+<!-- JS -->
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
+<script type='text/javascript' src='/js/main.js'></script>
+
+<script>
+</script>
--- bendoto/starbucks-stores.php
+++ bendoto/starbucks-stores.php
@@ -1,53 +1,11 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-	<title>Ben Ford – Product Designer</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-	<!-- Facebook / Google meta info -->
-	<link rel="canonical" href="http://www.website.com/" />
-	<meta property="og:locale" content="en_US" />
-	<meta property="og:type" content="website" />
-	<meta property="og:title" content="Ben Ford – Product Designer" />
-	<meta property="og:description" content="Portfolio of Ben Ford, a Product Designer" />
-	<meta property="og:url" content="http://www.website.com/" />
-	<meta property="og:site_name" content="Ben Ford – Product Designer" />
-	<meta property="og:image" content="/img/facebook-share.png" />
-
-	<!-- Twitter Meta info -->
-	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:site" content="http://www.website.com/" />
-	<meta name="twitter:title" content="Website name here" />
-	<meta name="twitter:description" content="Description of website here" />
-	<meta name="twitter:image" content="/img/facebook-share.png" />
-
-	<!-- Favicon -->
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/apple-touch-icon-152x152.png" />
-	<link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	<link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	<meta name="application-name" content="&nbsp;"/>
-	<meta name="msapplication-TileColor" content="#FFFFFF" />
-	<meta name="msapplication-TileImage" content="/img/mstile-144x144.png" />
-
-	<!-- Styles -->
-	<link rel="stylesheet" type="text/css" href="style/utility-classes.css" media="all">
-  <link rel="stylesheet" type="text/css" href="style/style.css" media="all">
-	<!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" media="all"> -->
-
-
-</head>
-
+<?php include 'head.php';?>
 <body>
 
 <!-- Project Hero Start -->
 			<section class="row salad-section py7 colorWhite">
-        <figure class="md-c7 md-last overflow-hidden sm-overflow-visible">
+        <figure class="md-c7 md-last">
           <img class="stores-hero" src="/img/web-app/phone-and-food-2.png","/bendoto/img/web-app/phone-and-food-2.png" alt="a delicious iced macchiatto">
         </figure>
         <article class="sm-c12 md-c5 mt4 md-mt16 vcenter">
@@ -94,28 +52,6 @@
         </article>
       </section>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <!-- JS -->
-<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
-<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
-<script type='text/javascript' src='/js/main.js'></script>
-
-<script>
-</script>
-
+<?php include 'javascript.php';?>
 </body>
 </html>
