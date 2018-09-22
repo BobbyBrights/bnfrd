6c75647384030e8a77ca87c5e440bed77443b2ec
Benjamin Ford <beford@beford665639.starbucks.net>
2018-09-22 15:14:44 -0700

    more work on homepage

--- index.php
+++ index.php
@@ -7,21 +7,21 @@
 <!-- Project Hero Start -->
 			<section class="home-row">
         <div class="tile-1 sm-c6 md-c4 md-offset-2">
-          <figure>
+          <figure><a href="#"></a>
             <img class="tiles" src="/img/home/tile-1.png" alt="a delicious iced macchiatto">
           </figure>
           <article class="relative">
-            <h2 class="heading-1">
+            <h2 class="home-heading-1">
               Search <span class="body-2 absolute">2018</span>
             </h2>
           </article>
         </div>
-        <div class="tile-2 sm-c6 sm-mt15 md-c3 md-offset-2">
-          <figure>
+        <div class="tile-2 sm-c6 sm-mt15 md-c3 md-offset-1">
+          <figure><a href="starbucks-web-app.php"></a>
             <img class="tiles" src="/img/home/tile-2.png" alt="a delicious iced macchiatto">
           </figure>
           <article class="relative">
-            <h2 class="heading-1">
+            <h2 class="home-heading-1">
               Starbucks.com <span class="body-2 absolute">2017</span>
             </h2>
           </article>
@@ -29,12 +29,12 @@
       </section>
 
       <section class="home-row">
-        <div class="tile-3 sm-c6 md-c4">
-          <figure>
+        <div class="tile-3 sm-c7 md-c4">
+          <figure><a href="starbucks-stores.php"></a>
             <img class="tiles" src="/img/home/tile-3.png" alt="a delicious iced macchiatto">
           </figure>
           <article class="relative">
-            <h2 class="heading-1">
+            <h2 class="home-heading-1">
               Stores <span class="body-2 absolute">2017</span>
             </h2>
           </article>
@@ -43,12 +43,35 @@
 
       <section class="home-row">
         <div class="tile-4 sm-c7 sm-offset-4 sm-mt3 md-c6 md-offset-5">
-          <figure>
+          <figure><a href="starbucks-design-system.php"></a>
             <img class="tiles" src="/img/home/tile-4.png" alt="a delicious iced macchiatto">
           </figure>
           <article class="relative">
-            <h2 class="heading-1">
-              Starbucks Design System <span class="body-2 absolute">2017</span>
+            <h2 class="home-heading-1">
+              Starbucks<br> Design<br> System <span class="body-2 absolute">2015</span>
+            </h2>
+          </article>
+        </div>
+      </section>
+
+			<section class="home-row bottom">
+        <div class="tile-5 sm-c5 md-c4 sm-offset-1">
+          <figure><a href="#"></a>
+            <img class="tiles" src="/img/home/tile-5.png" alt="a delicious iced macchiatto">
+          </figure>
+          <article class="relative">
+            <h2 class="home-heading-1">
+              Land O'Lakes Coop <span class="body-2 absolute">2014</span>
+            </h2>
+          </article>
+        </div>
+				<div class="tile-6 sm-c5 md-c4 md-offset-1">
+          <figure><a href="#"></a>
+            <img class="tiles" src="/img/home/tile-6.png" alt="a delicious iced macchiatto">
+          </figure>
+          <article class="relative">
+            <h2 class="home-heading-1">
+              Yes2You <span class="body-2 absolute">2014</span>
             </h2>
           </article>
         </div>
