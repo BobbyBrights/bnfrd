fd1135704e3ec75d57abf11c39726bab94f14724
Benjamin Ford <beford@beford665639.starbucks.net>
2018-10-22 12:43:09 -0700

    added product cell update

---
+++ sbux/product-cell-update/cart-layout.php
@@ -0,0 +1,233 @@
+<div class="cart-layout">
+  <div class="product-cell jjk c12">
+      <div class="title-row c12">
+        <div class="product-img">
+        </div>
+        <div class="row product-title  c12 nowrap vcenter">
+          <h1 class="sb-heading sb-heading--small">Latte Macchiato</h1>
+          <div class="total-price" id="total-price-150-original">$4.25
+          </div>
+          <div class="total-price hide" id="total-price-150">-150<span class="gold">★</span>
+          </div>
+        </div>
+      </div>
+      <div class="detail-row c12">
+        <div class="descriptor">Tall
+        </div>
+      </div>
+      <div class="detail-row c12 pt1">
+        <div class="descriptor">3 shots
+        </div>
+        <div class="dot" id="dot-150"></div>
+        <div class="price" id="price-150">+$0.90
+        </div>
+        <div class="price hide" id="price-25">-25<span class="gold">★</span>
+        </div>
+      </div>
+      <div class="detail-row c12 pt1">
+        <div class="descriptor">150<span class="gold">★</span> item
+        </div>
+      </div>
+      <div class="utility-row c12 pt1">
+        <div class="utility-actions">
+          <div class="heart"><a href="#"></a>
+            <div class="heart-hover"></div>
+              <div class="heart-img"></div>
+          </div>
+          <div class="plus"><a href="#"></a>
+            <div class="plus-hover"></div>
+              <div class="plus-img"></div>
+          </div>
+          <div class="minus"><a href="#"></a>
+            <div class="minus-hover"></div>
+              <div class="minus-img"></div>
+          </div>
+        </div>
+        <!-- <div class="total-price">$3.95
+        </div> -->
+      </div>
+    </div>
+    <hr>
+    <div class="product-cell c12">
+      <div class="title-row c12">
+        <div class="product-img2">
+        </div>
+        <div class="row product-title  c12 nowrap vcenter">
+          <h1 class="sb-heading sb-heading--small">
+            Double-Smoked Bacon, Cheddar & Egg Sandwich
+          </h1>
+          <div class="total-price">$4.95
+          </div>
+          <div class="total-price hide">-150<span class="gold">★</span>
+          </div>
+        </div>
+      </div>
+      <div class="detail-row c12 pt1">
+        <div class="descriptor">1 Sriracha packet
+        </div>
+      </div>
+
+      <div class="detail-row c12 pt1 pb2">
+        <div class="descriptor">150<span class="gold">★</span> item
+        </div>
+      </div>
+      <div class="utility-row c12 pt1">
+        <div class="utility-actions">
+          <div class="heart"><a href="#"></a>
+            <div class="heart-hover"></div>
+              <div class="heart-img"></div>
+          </div>
+          <div class="plus"><a href="#"></a>
+            <div class="plus-hover"></div>
+              <div class="plus-img"></div>
+          </div>
+          <div class="minus"><a href="#"></a>
+            <div class="minus-hover"></div>
+              <div class="minus-img"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="product-cell c12">
+      <div class="title-row c12">
+        <div class="product-img3">
+        </div>
+        <div class="row product-title  c12 nowrap vcenter">
+          <h1 class="sb-heading sb-heading--small">
+            Blonde Roast
+          </h1>
+          <div class="total-price" id="total-price-50-original">$2.45
+          </div>
+          <div class="total-price hide" id="total-price-50">-50<span class="gold">★</span>
+          </div>
+        </div>
+      </div>
+      <div class="detail-row c12">
+      </div>
+      <div class="detail-row c12 pt1 pb2">
+        <div class="descriptor">50<span class="gold">★</span> item
+        </div>
+      </div>
+      <div class="utility-row c12 pt1">
+        <div class="utility-actions">
+          <div class="heart"><a href="#"></a>
+            <div class="heart-hover"></div>
+              <div class="heart-img"></div>
+          </div>
+          <div class="plus"><a href="#"></a>
+            <div class="plus-hover"></div>
+              <div class="plus-img"></div>
+          </div>
+          <div class="minus"><a href="#"></a>
+            <div class="minus-hover"></div>
+              <div class="minus-img"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="product-cell c12">
+      <div class="title-row c12">
+        <div class="product-img4">
+        </div>
+        <div class="row product-title c12 nowrap vcenter">
+          <h1 class="sb-heading sb-heading--small">
+            Chai Créme Frappuccino®
+          </h1>
+          <div class="total-price">$4.95
+          </div>
+          <div class="total-price hide">-150<span class="gold">★</span>
+          </div>
+        </div>
+      </div>
+      <div class="detail-row c12">
+        <div class="descriptor">Grande
+        </div>
+      </div>
+      <!-- <div class="detail-row c12 pt1">
+        <div class="descriptor">2 shots
+        </div>
+        <div class="dot"></div>
+        <div class="price">+$1.80
+        </div>
+      </div> -->
+      <div class="detail-row c12 pt1">
+        <div class="descriptor">150<span class="gold">★</span> item
+        </div>
+      </div>
+      <div class="detail-row c12 pt2 pb0">
+        <div class="descriptor red-text bold">Sold out at this store
+        </div>
+        <div class="warning">
+        </div>
+      </div>
+      <div class="utility-row c12 pt1">
+        <div class="utility-actions">
+          <div class="heart"><a href="#"></a>
+            <div class="heart-hover"></div>
+              <div class="heart-img"></div>
+          </div>
+          <div class="plus"><a href="#"></a>
+            <div class="plus-hover"></div>
+              <div class="plus-img"></div>
+          </div>
+          <div class="minus"><a href="#"></a>
+            <div class="minus-hover"></div>
+              <div class="minus-img"></div>
+          </div>
+        </div>
+      </div>
+      <div class="rewards-row c12">
+      </div>
+    </div>
+    <hr>
+    <div class="product-cell">
+      <div class="ghost" id="25-action"><span class="reward-action">Use</span> 25<span class="gold">★</span>
+      </div>
+      <div class="ghost" id="50-action"><span class="reward-action">Use</span> 50<span class="gold">★</span>
+      </div>
+      <div class="ghost-special" id="150-action"><span class="reward-action">Use</span> 150<span class="gold">★</span>
+      </div>
+      <!-- <div class="reward-price">-$0.90</div>
+      <div class="reward-price-special">-$5.15</div> -->
+    </div>
+    <hr>
+    <div class="pricing-cell c12 mb4">
+      <div class="detail-row c12 pt1 pb2">
+        <div class="descriptor">Subtotal
+        </div>
+        <div class="dot"></div>
+        <div class="price">$5.47
+        </div>
+      </div>
+      <div class="detail-row c12 pb2">
+        <div class="descriptor">Tax 10.1%
+        </div>
+        <div class="dot"></div>
+        <div class="price">$1.18
+        </div>
+      </div>
+      <div class="detail-row c12 pb2">
+        <div class="descriptor">Tip
+        </div>
+        <div class="dot"></div>
+        <div class="price">$0.00
+        </div>
+      </div>
+      <div class="detail-row c12 pb2">
+        <div class="descriptor">Charity Roundup
+        </div>
+        <div class="dot"></div>
+        <div class="price">$0.50
+        </div>
+      </div>
+      <div class="detail-row c12">
+        <div class="descriptor large-text">Total
+        </div>
+        <div class="dot"></div>
+        <div class="price large-text">$27.00
+        </div>
+      </div>
+    </div>
+</div>
