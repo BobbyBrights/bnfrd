35cd6e33e24d4da4f4c9209914f15025f0669981
Benjamin Ford <beford@beford665639.starbucks.net>
2018-10-26 15:20:38 -0700

    added payment methods page

---
+++ sbux/payment-methods/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html>
+<?php include 'header.php';?>
+<body>
+
+<!-- Left Crate -->
+  <div class="container">
+		<?php include 'left-crate.php';?>
+
+		<!-- Right Crate -->
+		<div class="sb-contentCrate">
+			<div class="sb-contentCrateContainer wide c12">
+        <div class="payment-header p3 md-p0 lg-mt7 text-bold text-upper colorTextBlackSoft text-xxs">
+          My Payment
+        </div>
+        <div class="card bigCorner md-mt3">
+          <div class="c12 row p3 sm-p4 md-p3 standard-hover">
+            <div class="">
+              <img src="/img/add-button.svg" width="34" height="24" style="display:block">
+            </div>
+            <div class="ml3 vcenter text-sm">
+              Add credit card
+            </div>
+          </div>
+        </div>
+
+        <div class="payment-header p3 md-p0 mt7 text-bold text-upper colorTextBlackSoft text-xxs">
+          add new payment
+        </div>
+        <div class="card bigCorner md-mt3">
+          <div class="c12 row p3 sm-p4 md-p3 standard-hover">
+            <div class="">
+              <img src="/img/visa-checkout.png" width="34" height="24" style="display:block">
+            </div>
+            <div class="ml3 vcenter text-sm">
+              Card from Visa Checkout
+            </div>
+          </div>
+          <hr>
+          <div class="c12 row p3 sm-p4 md-p3 standard-hover">
+            <div class="">
+              <img src="/img/chase.png" width="34" height="24" style="display:block">
+            </div>
+            <div class="ml3 vcenter text-sm">
+              Chase Pay
+            </div>
+          </div>
+          <hr>
+          <div class="c12 row p3 sm-p4 md-p3 standard-hover">
+            <div class="">
+              <img src="/img/paypal.png" width="34" height="24" style="display:block">
+            </div>
+            <div class="ml3 vcenter text-sm">
+              Paypal
+            </div>
+          </div>
+        </div>
+
+      </div>
+		</div>
+  </div>
+
+<?php include 'footer.php';?>
