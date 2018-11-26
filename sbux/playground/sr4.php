ca95d9454afe55d6533c7d77ad8c3f190025c93e
Benjamin Ford <beford@beford665639.starbucks.net>
2018-11-26 11:36:09 -0800

    rearranged folder structure, more work on sr4 and sr3

--- sbux/playground/sr4.php
+++ sbux/playground/sr4.php
@@ -16,7 +16,7 @@
 		<div class="sb-contentCrate">
 			<div class="sb-contentCrateContainer">
         <div class="wide-row">
-          <div class="basic-card mt3 pt3 px3 pb5 sm-pt5 sm-pr10 sm-pb7 md-pt4 md-pr9 md-pb6 md-pl4 lg-pr13">
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
             <div class="title row nowrap c12 middle">
               <div class="product-img latte-macchiato circle-div">
               </div>
@@ -24,19 +24,19 @@
                 <h2 class="title-2 pl4">
                   Latte Macchiato
                 </h2>
-                <h3 class="subhead-2 ml-auto vend">
+                <h3 class="subhead-2 pb1 ml-auto vend">
                   $4.25
                 </h3>
               </div>
             </div>
             <div class="product-details c12 pl11 md-pl15">
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 Tall
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 3 shots
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 125<span class="colorTextGold text-bold">☆</span> item
               </h3>
               <div class="row pl1">
@@ -44,13 +44,17 @@
                 <div class="icon-button add"></div>
                 <div class="icon-button minus"></div>
               </div>
-              <div class="pl3 row nowrap reward-message hide" id="125-option">
-                <p class="colorTextGreen button-text">It’s on us! 125 Star Reward applied.</p>
-                <p class="button-text colorTextBlack ml-auto">-25<span class="colorTextGold">☆</p>
+              <div class="pl3 row nowrap reward-message hide" id="25-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 25 Star Reward applied.</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-25<span class="colorTextGold">☆</p>
+              </div>
+              <div class="pl3 row nowrap reward-message hide" id="150-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 150 Star Reward applied.</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-150<span class="colorTextGold">☆</p>
               </div>
             </div>
           </div>
-          <div class="basic-card mt3 pt3 px3 pb5 sm-pt5 sm-pr10 sm-pb7 md-pt4 md-pr9 md-pb6 md-pl4 lg-pr13">
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
             <div class="title row nowrap c12 middle">
               <div class="product-img double-smoked-bacon circle-div">
               </div>
@@ -58,19 +62,19 @@
                 <h2 class="title-2 pl4">
                   Double-Smoked Bacon, Cheddar & Egg Sandwich
                 </h2>
-                <h3 class="subhead-2 ml-auto vend">
+                <h3 class="subhead-2 pb1 ml-auto vend">
                   $4.25
                 </h3>
               </div>
             </div>
-            <div class="product-details pl11 md-pl15">
-              <h3 class="body-1 pl3 md-pl3">
+            <div class="product-details c12 pl11 md-pl15">
+              <h3 class="body-1 pl3">
                 Tall
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 3 shots
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 125<span class="colorTextGold text-bold">☆</span> item
               </h3>
               <div class="row pl1">
@@ -78,9 +82,13 @@
                 <div class="icon-button add"></div>
                 <div class="icon-button minus"></div>
               </div>
+              <div class="pl3 row nowrap reward-message hide" id="200-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 200 Star Reward applied.</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-200<span class="colorTextGold">☆</p>
+              </div>
             </div>
           </div>
-          <div class="basic-card mt3 pt3 px3 pb5 sm-pt5 sm-pr10 sm-pb7 md-pt4 md-pr9 md-pb6 md-pl4 lg-pr13">
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
             <div class="title row nowrap c12 middle">
               <div class="product-img blonde-roast circle-div">
               </div>
@@ -88,19 +96,19 @@
                 <h2 class="title-2 pl4">
                   Blonde Roast
                 </h2>
-                <h3 class="subhead-2 ml-auto vend">
+                <h3 class="subhead-2 pb1 ml-auto vend">
                   $4.25
                 </h3>
               </div>
             </div>
-            <div class="product-details pl11 md-pl15">
-              <h3 class="body-1 pl3 md-pl3">
+            <div class="product-details c12 pl11 md-pl15">
+              <h3 class="body-1 pl3">
                 Tall
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 3 shots
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 125<span class="colorTextGold text-bold">☆</span> item
               </h3>
               <div class="row pl1">
@@ -108,9 +116,13 @@
                 <div class="icon-button add"></div>
                 <div class="icon-button minus"></div>
               </div>
+              <div class="pl3 row nowrap reward-message hide" id="50-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 50 Star Reward applied.</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-50<span class="colorTextGold">☆</p>
+              </div>
             </div>
           </div>
-          <div class="basic-card mt3 pt3 px3 pb5 sm-pt5 sm-pr10 sm-pb7 md-pt4 md-pr9 md-pb6 md-pl4 lg-pr13">
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
             <div class="title row nowrap c12 middle">
               <div class="product-img chai-creme-frap circle-div">
               </div>
@@ -118,19 +130,19 @@
                 <h2 class="title-2 pl4">
                   Chai Créme Frappuccino®
                 </h2>
-                <h3 class="subhead-2 ml-auto vend">
+                <h3 class="subhead-2 pb1 ml-auto vend">
                   $4.25
                 </h3>
               </div>
             </div>
-            <div class="product-details pl11 md-pl15">
-              <h3 class="body-1 pl3 md-pl3">
+            <div class="product-details c12 pl11 md-pl15">
+              <h3 class="body-1 pl3">
                 Tall
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 3 shots
               </h3>
-              <h3 class="body-1 pl3 md-pl3">
+              <h3 class="body-1 pl3">
                 125<span class="colorTextGold text-bold">☆</span> item
               </h3>
               <div class="row pl1">
@@ -138,11 +150,27 @@
                 <div class="icon-button add"></div>
                 <div class="icon-button minus"></div>
               </div>
+              <div class="pl3 row nowrap reward-message hide" id="400-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 400 Star Reward applied.</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-400<span class="colorTextGold">☆</p>
+              </div>
             </div>
           </div>
-          <div class="basic-card mb4 mt3 py3 px3 md-pl4">
-            <div class="filled-button black toggle-reward" id="125-reward">
-              <span class="reward-text">Use</span> 125 <span class="colorTextGold">★</span>
+          <div class="product-card mb4 p2 sm-p4 md-py3 md-mt3 md-pl4">
+            <div class="filled-button black toggle-reward m2" id="25-reward">
+              <span class="reward-text">Use</span> 25 <span class="colorTextGold">★</span>
+            </div>
+            <div class="filled-button black toggle-reward m2" id="50-reward">
+              <span class="reward-text">Use</span> 50 <span class="colorTextGold">★</span>
+            </div>
+            <div class="filled-button black toggle-reward m2" id="150-reward">
+              <span class="reward-text">Use</span> 150 <span class="colorTextGold">★</span>
+            </div>
+            <div class="filled-button black toggle-reward m2" id="200-reward">
+              <span class="reward-text">Use</span> 200 <span class="colorTextGold">★</span>
+            </div>
+            <div class="filled-button black toggle-reward m2" id="400-reward">
+              <span class="reward-text">Use</span> 400 <span class="colorTextGold">★</span>
             </div>
           </div>
         </div>
