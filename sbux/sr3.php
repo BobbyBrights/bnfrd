3b5c649750472a8d01b61303d42d87aaccaba871
Benjamin Ford <beford@beford665639.starbucks.net>
2018-12-06 11:30:48 -0800

    dsfsdf

--- sbux/sr3.php
+++ sbux/sr3.php
@@ -131,48 +131,48 @@
           <div class="caption-2 colorTextBlackSoft mt4 mb1 ml3 sm-mt4 sm-ml5 md-ml0 md-mt5 c12">
             Would you like to use a Reward?
           </div>
-          <div class="product-card mb3 p3 pb4 sm-p4 sm-pl5 sm-pt3 md-py3 md-mt2 md-pl4">
-            <div class="filled-button black toggle-reward" id="125-reward">
+          <div class="product-card mb2 md-mb3 p3 pl2 pb4 sm-p4 sm-pl4 sm-pt3 md-py4 md-mt2 md-pl4">
+            <div class="filled-button black toggle-reward m2" id="125-reward">
               <span class="reward-text">Use</span> 125 <span class="colorTextGold">★</span>
             </div>
           </div>
           <div class="product-card mb15 px3 pb5 pt3 sm-pt3 sm-px5 sm-pb5 md-pt4 md-pb5">
-            <div class="row c12 px2 pt1 pb1 hide colorTextGreen" id="125-reward-total">
+            <div class="row c12 pt1 pb1 hide colorTextGreen" id="125-reward-total">
               <div class="body-1">Discount
               </div>
               <div class="dottedUnderline"></div>
               <div class="subhead-2 ml-auto">-$0.90
               </div>
             </div>
-            <div class="row c12 px2 pt1 pb2">
+            <div class="row c12 pt1 pb2">
               <div class="body-1 colorTextBlackSoft">Subtotal
               </div>
               <div class="dottedUnderline"></div>
               <div class="subhead-2 ml-auto">$29.00
               </div>
             </div>
-            <div class="row c12 px2 pb2">
+            <div class="row c12 pb2">
               <div class="body-1 colorTextBlackSoft">Tax 10.1%
               </div>
               <div class="dottedUnderline"></div>
               <div class="subhead-2 ml-auto">$2.93
               </div>
             </div>
-            <div class="row c12 px2 pb2">
+            <div class="row c12 pb2">
               <div class="body-1 colorTextBlackSoft">Tip
               </div>
               <div class="dottedUnderline"></div>
               <div class="subhead-2 ml-auto">$0.00
               </div>
             </div>
-            <div class="row c12 px2 pb2">
+            <div class="row c12 pb2">
               <div class="body-1 colorTextBlackSoft">Charity Roundup
               </div>
               <div class="dottedUnderline"></div>
               <div class="subhead-2 ml-auto">$0.00
               </div>
             </div>
-            <div class="row c12 px2">
+            <div class="row c12">
               <div class="frap-text large-text">Total
               </div>
               <div class="dottedUnderline"></div>
