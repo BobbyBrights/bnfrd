951524fd35169c0e52b3964460d0ffb452e8da0e
Benjamin Ford <beford@beford665639.starbucks.net>
2019-02-13 13:01:09 -0800

    added sr4-rewards-message page

---
+++ sbux/sr4-rewards-message.php
@@ -0,0 +1,338 @@
+<!DOCTYPE HTML>
+<html>
+<?php include 'header.php';?>
+<body>
+
+<!-- Left Crate -->
+  <div class="container">
+    <div class="sb-headerCrate colorBlackWarmer">
+      <div class="sb-navbar">
+          <div class="sb-navbar-siren vcenter"></div>
+      </div>
+      <div class="greeting sb-heading sb-heading--large text-bold colorTextWhite">Review order</div>
+    </div>
+
+		<!-- Right Crate -->
+		<div class="sb-contentCrate">
+			<div class="sb-contentCrateContainer">
+        <div class="wide-row">
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
+            <div class="title row nowrap c12 middle">
+              <div class="product-img latte-macchiato circle-div">
+              </div>
+              <div class="row c12 middle nowrap">
+                <h2 class="title-2 pl4">
+                  Latte Macchiato
+                </h2>
+                <h3 class="subhead-2 pb1 ml-auto vend">
+                  $4.25
+                </h3>
+              </div>
+            </div>
+            <div class="product-details c12 pl11 md-pl15">
+              <h3 class="body-1 pl3">
+                Tall
+              </h3>
+              <div class="row c12">
+                <h3 class="body-1 pl3">
+                  3 shots
+                </h3>
+                <div class="dottedUnderline"></div>
+                <h3 class="subhead-2">
+                  +$0.90
+                </h3>
+              </div>
+              <h3 class="body-1 pl3">
+                150<span class="colorTextGold text-bold">☆</span> item
+              </h3>
+              <div class="row pl1">
+                <div class="icon-button heart"></div>
+                <div class="icon-button add"></div>
+                <div class="icon-button minus"></div>
+              </div>
+              <div class="pl3 row nowrap reward-message hide" id="25-reward-message">
+                <p class="colorTextGreen button-text">25 Star Reward applied toward your drink customizations</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-25<span class="colorTextGold">☆</p>
+              </div>
+              <div class="pl3 row nowrap reward-message hide" id="150-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 150 Star Reward applied</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-150<span class="colorTextGold">☆</p>
+              </div>
+            </div>
+          </div>
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
+            <div class="title row nowrap c12 middle">
+              <div class="product-img grilled-chicken-salad circle-div">
+              </div>
+              <div class="row c12 middle nowrap">
+                <h2 class="title-2 pl4">
+                  Grilled Chicken & Cauliflower Tabbouleh
+                </h2>
+                <h3 class="subhead-2 pb1 ml-auto vend">
+                  $9.45
+                </h3>
+              </div>
+            </div>
+            <div class="product-details c12 pl11 md-pl15">
+              <h3 class="body-1 pl3">
+                1 piece
+              </h3>
+              <h3 class="body-1 pl3">
+                200<span class="colorTextGold text-bold">☆</span> item
+              </h3>
+              <div class="row pl1">
+                <div class="icon-button heart"></div>
+                <div class="icon-button add"></div>
+                <div class="icon-button minus"></div>
+              </div>
+              <div class="pl3 row nowrap reward-message hide" id="200-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 200 Star Reward applied</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-200<span class="colorTextGold">☆</p>
+              </div>
+            </div>
+          </div>
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
+            <div class="title row nowrap c12 middle">
+              <div class="product-img blonde-roast circle-div">
+              </div>
+              <div class="row c12 middle nowrap">
+                <h2 class="title-2 pl4">
+                  Blonde Roast
+                </h2>
+                <h3 class="subhead-2 pb1 ml-auto vend">
+                  $2.45
+                </h3>
+              </div>
+            </div>
+            <div class="product-details c12 pl11 md-pl15">
+              <h3 class="body-1 pl3">
+                Grande
+              </h3>
+              <h3 class="body-1 pl3">
+                50<span class="colorTextGold text-bold">☆</span> item
+              </h3>
+              <div class="row pl1">
+                <div class="icon-button heart"></div>
+                <div class="icon-button add"></div>
+                <div class="icon-button minus"></div>
+              </div>
+              <div class="pl3 row nowrap reward-message hide" id="50-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 50 Star Reward applied</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-50<span class="colorTextGold">☆</p>
+              </div>
+            </div>
+          </div>
+          <div class="product-card px3 pt3 pb4 sm-p5 md-mt3 md-pr13">
+            <div class="title row nowrap c12 middle">
+              <div class="product-img pods circle-div">
+              </div>
+              <div class="row c12 middle nowrap">
+                <h2 class="title-2 pl4">
+                  Espresso Roast Verismo® Pods
+                </h2>
+                <h3 class="subhead-2 pb1 ml-auto vend">
+                  $11.95
+                </h3>
+              </div>
+            </div>
+            <div class="product-details c12 pl11 md-pl15">
+              <h3 class="body-1 pl3">
+                12 count
+              </h3>
+              <h3 class="body-1 pl3">
+                400<span class="colorTextGold text-bold">☆</span> item
+              </h3>
+              <div class="row pl1">
+                <div class="icon-button heart"></div>
+                <div class="icon-button add"></div>
+                <div class="icon-button minus"></div>
+              </div>
+              <div class="pl3 row nowrap reward-message hide" id="400-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! 400 Star Reward applied</p>
+                <p class="button-text colorTextBlack ml-auto pl3">-400<span class="colorTextGold">☆</p>
+              </div>
+              <div class="pl3 row nowrap reward-message hide" id="birthday-reward-message">
+                <p class="colorTextGreen button-text">It’s on us! Birthday Reward applied</p>
+              </div>
+            </div>
+          </div>
+          <div class="product-card mt2 md-my3 md-p0">
+            <div class="row c12 border-bottom-none md-border-bottom px3 sm-px5 sm-mt2 md-pb2">
+              <div class="subhead-2 vcenter">Reward options</div>
+              <div class="icon-button ml-auto info initiate-top-sheet"></div>
+            </div>
+            <div class="row px2 mt2 mb3 sm-px4 sm-mb5 md-my3">
+              <div class="filled-button black toggle-reward m2" id="25-reward">
+                <span class="reward-text">Use</span> 25 <span class="colorTextGold">★</span>
+              </div>
+              <div class="filled-button black toggle-reward m2" id="50-reward">
+                <span class="reward-text">Use</span> 50 <span class="colorTextGold">★</span>
+              </div>
+              <div class="filled-button black toggle-reward m2" id="150-reward">
+                <span class="reward-text">Use</span> 150 <span class="colorTextGold">★</span>
+              </div>
+              <div class="filled-button black toggle-reward m2" id="200-reward">
+                <span class="reward-text">Use</span> 200 <span class="colorTextGold">★</span>
+              </div>
+              <div class="filled-button black toggle-reward m2" id="400-reward">
+                <span class="reward-text">Use</span> 400 <span class="colorTextGold">★</span>
+              </div>
+              <div class="filled-button black toggle-reward m2" id="birthday-reward">
+                <span class="reward-text">Use</span> Birthday Reward
+              </div>
+            </div>
+          </div>
+          <div class="product-card mb15 px3 pb5 pt3 sm-pt3 sm-px5 sm-pb5 md-pt4 md-pb5">
+            <div class="row c12 pt1 pb1 hide reward-discount colorTextGreen" id="25-reward-total">
+              <div class="body-1">Discount
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">-$0.90
+              </div>
+            </div>
+            <div class="row c12 pt1 pb1 hide reward-discount colorTextGreen" id="50-reward-total">
+              <div class="body-1">Discount
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">-$2.45
+              </div>
+            </div>
+            <div class="row c12 pt1 pb1 hide reward-discount colorTextGreen" id="150-reward-total">
+              <div class="body-1">Discount
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">-$4.25
+              </div>
+            </div>
+            <div class="row c12 pt1 pb1 hide reward-discount colorTextGreen" id="200-reward-total">
+              <div class="body-1">Discount
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">-$9.45
+              </div>
+            </div>
+            <div class="row c12 pt1 pb1 hide reward-discount colorTextGreen" id="400-reward-total">
+              <div class="body-1">Discount
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">-$11.95
+              </div>
+            </div>
+            <div class="row c12 pt1 pb1 hide reward-discount colorTextGreen" id="birthday-reward-total">
+              <div class="body-1">Discount
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">-$11.95
+              </div>
+            </div>
+            <div class="row c12 pt1 pb2">
+              <div class="body-1 colorTextBlackSoft">Subtotal
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">$29.00
+              </div>
+            </div>
+            <div class="row c12 pb2">
+              <div class="body-1 colorTextBlackSoft">Tax 10.1%
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">$2.93
+              </div>
+            </div>
+            <div class="row c12 pb2">
+              <div class="body-1 colorTextBlackSoft">Tip
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">$0.00
+              </div>
+            </div>
+            <div class="row c12 pb2">
+              <div class="body-1 colorTextBlackSoft">Charity Roundup
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="subhead-2 ml-auto">$0.00
+              </div>
+            </div>
+            <div class="row c12">
+              <div class="frap-text large-text">Total
+              </div>
+              <div class="dottedUnderline"></div>
+              <div class="frap-text ml-auto large-text">$31.93
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="basic-toast middle">
+          <p class="pr3">125<span class="colorTextGold">★</span> applied to Latte Macchiato</p>
+          <div class="ghost-button white ml-auto invisible">Undo</div>
+        </div>
+        <div class="frap-container">
+          <div class="frap">
+            Checkout $31.93
+          </div>
+        </div>
+			</div>
+		</div>
+  </div>
+
+  <div class="top-sheet fixed">
+    <div class="row colorWhite nowrap middle between fixed p2 m1 l0 r0 z-1 lg-l-40p lg-m0 lg-p4 lg-m1">
+      <div class="icon-button close dismiss-top-sheet first sm-last z-1"></div>
+      <div class="subhead-2 centered-text vcenter ml6 sm-ml0 sm-mr6">Rewards & offers</div>
+      <div class="pl2 row middle last sm-first">
+        <div class="subhead-2 sm-ml0">174</div>
+        <div class="icon small-star ml-neg2"></div>
+      </div>
+    </div>
+    <div class="row column lg-mt0 wide-row mt10 sm-mt12 lg-mt14">
+      <div class="colorCeramic p3 border-radius-6 mx3 md-mx0 row">
+        <div class="title-1 mb4">
+          Tell your barista you’d like to use a Reward or offer.
+        </div>
+        <div class="grid grid-row-gap-16">
+          <div class="grid-column-1 subhead-2">
+            25<span class="colorTextGold ml1">★</span>
+          </div>
+          <div class="grid-column-2 body-1 ml3">
+            Customize your drink (espresso shot, dairy substitute, syrup and more)
+          </div>
+          <div class="grid-column-1 subhead-2">
+            50<span class="colorTextGold ml1">★</span>
+          </div>
+          <div class="grid-column-2 body-1 ml3">
+            Brewed hot coffee, bakery item or hot tea
+          </div>
+          <div class="grid-column-1 subhead-2">
+            150<span class="colorTextGold ml1">★</span>
+          </div>
+          <div class="grid-column-2 body-1 ml3">
+            Handcrafted drink, hot breakfast or parfait
+          </div>
+          <div class="grid-column-1 subhead-2">
+            200<span class="colorTextGold ml1">★</span>
+          </div>
+          <div class="grid-column-2 body-1 ml3">
+            Lunch sandwich, protein box or salad
+          </div>
+          <div class="grid-column-1 subhead-2">
+            400<span class="colorTextGold ml1">★</span>
+          </div>
+          <div class="grid-column-2 body-1 ml3">
+            Select merchandise or at-home coffee
+          </div>
+        </div>
+      </div>
+      <div class="colorCeramic p3 border-radius-6 mx3 md-mx0 mt2 row nowrap top">
+        <div class="colorBlackWarmer p4 pl5 pt5"></div>
+        <div class="ml3">
+          <div class="subhead-2">Birthday Reward</div>
+          <div class="body-1 mt1">Lorem ipsum dolor sit amet, omnis utinam laudem cum eu, docendi.</div>
+        </div>
+      </div>
+      <div class="filled-button black inline-block mt4 vcenter dismiss-top-sheet mb4">
+        Done
+      </div>
+    </div>
+  </div>
+
+  <?php include 'component-logic.php';?>
