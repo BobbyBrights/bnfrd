54d21dc06558dc7e0f3a18200f0f03369bf45a20
Ben <ford.ben.90@gmail.com>
2019-03-18 11:35:44 -0700

    hover states add to home page

--- hidden/index.php
+++ hidden/index.php
@@ -8,89 +8,103 @@
 <!-- Project Hero Start -->
 	<section class="home-row">
     <div class="tile-1 sm-c6 md-c4 md-offset-2">
-      <figure class="relative"><a href="#">
-				<h3 class="caption colorTextWhitePrimary absolute rotate-90">Product Design</h3>
-        <img class="tiles" src="/hidden/img/0-home/tile-search.jpg" alt="a delicious iced macchiatto"></a>
-      </figure>
-      <article class="relative">
-        <h2 class="home-heading-1">
-          Search <span class="body-2 absolute">2018</span>
-        </h2>
-      </article>
+			<div class="tile">
+	      <figure class="overflow-hidden relative"><a href="#">
+					<h3 class="caption colorTextWhitePrimary absolute rotate-90 z-3">Product Design</h3>
+	        <img class="tiles" src="/hidden/img/0-home/tile-search.jpg" alt="a delicious iced macchiatto"></a>
+	      </figure>
+	      <article class="relative cursor-pointer">
+	        <h2 class="home-heading-1">
+	          Search <span class="body-2 absolute">2018</span>
+	        </h2>
+	      </article>
+			</div>
     </div>
     <div class="tile-2 sm-c6 sm-mt15 md-c3 md-offset-1">
-      <figure><a href="starbucks-web-app.php">
-        <img class="tiles" src="/hidden/img/0-home/tile-web-app.jpg" alt="a delicious iced macchiatto"></a>
-      </figure>
-      <article class="relative">
-        <h2 class="home-heading-1">
-          Starbucks.com <span class="body-2 absolute">2017</span>
-        </h2>
-      </article>
+			<div class="tile">
+	      <figure class="overflow-hidden"><a href="starbucks-web-app.php">
+	        <img class="tiles" src="/hidden/img/0-home/tile-web-app.jpg" alt="a delicious iced macchiatto"></a>
+	      </figure>
+	      <article class="relative cursor-pointer">
+	        <h2 class="home-heading-1">
+	          Starbucks.com <span class="body-2 absolute">2017</span>
+	        </h2>
+	      </article>
+			</div>
     </div>
   </section>
 
   <section class="home-row">
     <div class="tile-3 sm-c7 md-c4">
-      <figure><a href="new-stores.php">
-        <img class="tiles" src="/hidden/img/0-home/tile-stores.jpg" alt="a delicious iced macchiatto"></a>
-      </figure>
-      <article class="relative">
-        <h2 class="home-heading-1">
-          Stores <span class="body-2 absolute">2017</span>
-        </h2>
-      </article>
+			<div class="tile">
+	      <figure class="overflow-hidden"><a href="new-stores.php">
+	        <img class="tiles" src="/hidden/img/0-home/tile-stores.jpg" alt="a delicious iced macchiatto"></a>
+	      </figure>
+	      <article class="relative cursor-pointer">
+	        <h2 class="home-heading-1">
+	          Stores <span class="body-2 absolute">2017</span>
+	        </h2>
+	      </article>
+			</div>
     </div>
   </section>
 
 	<section class="home-row">
     <div class="sm-c7 md-c4 md-offset-5 md-mt-neg16">
-      <figure><a href="new-stores.php">
-        <img class="tiles" src="/hidden/img/0-home/tile-gift.jpg" alt="a delicious iced macchiatto"></a>
-      </figure>
-      <article class="relative">
-        <h2 class="home-heading-1">
-          Gift <span class="body-2 absolute">2017</span>
-        </h2>
-      </article>
+			<div class="tile">
+	      <figure class="overflow-hidden"><a href="new-stores.php">
+	        <img class="tiles" src="/hidden/img/0-home/tile-gift.jpg" alt="a delicious iced macchiatto"></a>
+	      </figure>
+	      <article class="relative cursor-pointer">
+	        <h2 class="home-heading-1">
+	          Gift <span class="body-2 absolute">2017</span>
+	        </h2>
+	      </article>
+			</div>
     </div>
   </section>
 
   <section class="home-row">
     <div class="sm-c7 sm-offset-3 sm-mt3 md-c5 md-offset-3 md-mt12">
-			<a href="starbucks-design-system.php">
-      <figure>
-        <img class="tiles" src="/img/home/tile-4.png" alt="a delicious iced macchiatto">
-      </figure>
-			</a>
-      <article class="relative">
-        <h2 class="home-heading-1">
-          Starbucks Design System <span class="body-2 absolute">2015</span>
-        </h2>
-      </article>
+			<div class="tile">
+				<a href="starbucks-design-system.php">
+	      <figure class="overflow-hidden">
+	        <img class="tiles" src="/img/home/tile-4.png" alt="a delicious iced macchiatto">
+	      </figure>
+				</a>
+	      <article class="relative cursor-pointer">
+	        <h2 class="home-heading-1">
+	          Starbucks Design System <span class="body-2 absolute">2015</span>
+	        </h2>
+	      </article>
+			</div>
     </div>
   </section>
 
 	<section class="home-row bottom md-mt12">
     <div class="sm-c5 md-c4 md-offset-1">
-      <figure><a href="#">
-        <img class="tiles" src="/hidden/img/0-home/tile-lol.jpg" alt="a delicious iced macchiatto"></a>
-      </figure>
-      <article class="relative">
-        <h2 class="home-heading-1">
-          Land O'Lakes Coop <span class="body-2 absolute">2014</span>
-        </h2>
-      </article>
+			<div class="tile">
+	      <figure class="overflow-hidden"><a href="#">
+	        <img class="tiles" src="/hidden/img/0-home/tile-lol.jpg" alt="a delicious iced macchiatto"></a>
+	      </figure>
+	      <article class="relative cursor-pointer">
+	        <h2 class="home-heading-1">
+	          Land O'Lakes Coop <span class="body-2 absolute">2014</span>
+	        </h2>
+	      </article>
+			</div>
     </div>
 		<div class="sm-c5 md-c4 md-offset-1 md-mt16 md-pt10">
-      <figure><a href="#">
-        <img class="tiles" src="/img/home/tile-6.png" alt="a delicious iced macchiatto"></a>
-      </figure>
-      <article class="relative">
-        <h2 class="home-heading-1">
-          Yes2You <span class="body-2 absolute">2014</span>
-        </h2>
-      </article>
+			<div class="tile">
+	      <figure class="tile overflow-hidden"><a href="#">
+	        <img class="tiles" src="/img/home/tile-6.png" alt="a delicious iced macchiatto"></a>
+	      </figure>
+	      <article class="relative cursor-pointer">
+	        <h2 class="home-heading-1">
+	          Yes2You <span class="body-2 absolute">2014</span>
+	        </h2>
+	      </article>
+			</div>
     </div>
   </section>
 
