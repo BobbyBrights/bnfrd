810d3d9128b27e02c2ff6147ab714c3022d9dba9
Ben <ford.ben.90@gmail.com>
2019-03-23 12:23:06 -0700

    Updated about page

---
+++ portfolio/about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<?php include 'head.php';?>
+<body>
+<?php include 'nav.php';?>
+<!--
+<div class="row">
+	<
+	<img class="" src="img/portrait.jpg" alt="picture" />
+</div> -->
+
+<section class="flex md-m-auto max-width-1268 md-py0 mt10 sm-mt15p md-mt0 md-height-100vw justify-content-center colorWhite">
+	<figure class="flex justify-content-center sm-c12 md-c5 md-mt0 md-last align-self-center">
+		<img class="width-100 align-self-center sm-width-50 md-width-100 circle" src="img/portrait.jpg" alt="picture">
+	</figure>
+	<article class="mt6 sm-c9 md-c7 md-mt0 md-align-self-center">
+		<!-- <h3 class="caption colorPink pb3 md-pb4">
+			about ben
+		</h3> -->
+		<p class="subhead colorTextSecondary">
+			Enjoys 39¢ wings. Name sounds like a president from the 1800’s. Cooks by the book. Spends too much time playing pinball. Attempts to write music. Full name is Benjamin Harrison Ford. Is now a cat person. Terrible plant owner. <br><br><br>I’m currently based in Seattle as a Product Designer at <a href="http://app.starbucks.com"><i><mark>Starbucks</mark></a></i>. Previously at Deloitte Digital.</h3>
+		</p>
+	</article>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- JS -->
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
+<script type='text/javascript' src='/js/main.js'></script>
+
+<!-- Analytics -->
+
+</body>
+</html>
--- portfolio/nav.php
+++ portfolio/nav.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="z-3">
   <ul class="py4 sm-py6 flex">
     <li class="body-1 colorTextPrimary pl4 sm-pl5">
       <a href="index.php">
@@ -6,7 +6,7 @@
       </a>
     </li>
     <li class="body-1 colorTextPrimary pr4 sm-pr5 ml-auto">
-      <a href="#">
+      <a href="about.php">
         About
       </a>
     </li>
